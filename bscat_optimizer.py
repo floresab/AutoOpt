76d1b83e25afd19fa9f06d30970f6fdc438983b4
Lydia Mazeeva <l.mazeeva@wustl.edu>
2025-07-02 00:11:11 -0700

    pass spatial sym and bin as param

--- bscat_optimizer.py
+++ bscat_optimizer.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import shutil
 import sys
 import os
 import copy
@@ -11,18 +13,9 @@ from control import Control
 from deck import read_params_and_deck
 from AutoOpt import opt_E
 
-# --- Parameters ---
-# target_energy = -23.8819
-# slope_max = 2
-# input_db = 0.01152 # 15% b_0
-# input_de = 0.25
-# Emin = 0.5
-# Emax = 5.5
-# Estart = 3
-# b_0 = 0.0768
-
 # --- Helper Functions ---
-def estimate_initial_db_de(b_0, input_db, control_file, deck, deck_file, target_energy):
+def estimate_initial_db_de(b_0, ss, input_db, control_file, deck, deck_file, target_energy, BIN_PATH):
+    print("input_db =", input_db)
     b_plus = b_0 + input_db
     b_minus = b_0 - input_db
 
@@ -33,7 +26,7 @@ def estimate_initial_db_de(b_0, input_db, control_file, deck, deck_file, target_
     forward_file = control_forward.Name + ".ctrl"
 
     print("FORWARD (b_minus)")
-    E_minus, *_ = opt_E(b_minus, forward_file, target_energy, "scratch")
+    E_minus, *_ = opt_E(b_minus, ss, forward_file, target_energy, "scratch", BIN_PATH)
     deck.write_deck(deck_file.strip(".dk"))
 
     control_backward = Control()
@@ -43,10 +36,12 @@ def estimate_initial_db_de(b_0, input_db, control_file, deck, deck_file, target_
     backward_file = control_backward.Name + ".ctrl"
     
     print("BACKWARD (b_plus)")
-    E_plus, *_ = opt_E(b_plus, backward_file, target_energy, "scratch")
+    E_plus, *_ = opt_E(b_plus, ss, backward_file, target_energy, "scratch", BIN_PATH)
     deck.write_deck(deck_file.strip(".dk"))
 
     db_de = (b_plus - b_minus) / (E_plus - E_minus)
+    if db_de > 0:
+        db_de *= -1
     return db_de, b_plus, E_plus, b_minus, E_minus, backward_file, forward_file
 
 def update_db_de(b_new, b_prev, E_new, E_prev):
@@ -56,11 +51,9 @@ def log_step_info(step, direction, bscat, db_de, E_next):
     print(f"[{direction} Step {step}] bscat = {bscat:.5f}, db/de = {db_de:.5f}, E_rel = {E_next:.5f}")
 
 
-
-
-def run_bscat_scan(control_file, b_0, input_db, input_de, Emin, Emax, Estart, target_energy, slope_max):
+def run_bscat_scan(control_file, b_0, ss, input_db, input_de, E_min, E_max, E_start, target_energy, slope_max, BIN_PATH):
     
-    E_0, var_0, path_0, wse_0 = opt_E(b_0, control_file, target_energy, "scratch")
+    E_0, var_0, path_0, wse_0 = opt_E(b_0, ss, control_file, target_energy, "scratch", BIN_PATH)
 
     scattering_control = Control()
     scattering_control.read_control(control_file)
@@ -78,7 +71,7 @@ def run_bscat_scan(control_file, b_0, input_db, input_de, Emin, Emax, Estart, ta
     }]
 
     db_de_initial, b_plus, E_plus, b_minus, E_minus, backward_ctrl_file, forward_ctrl_file = estimate_initial_db_de(
-        b_0, input_db, control_file, deck, deck_file, target_energy)
+        b_0, ss, input_db, control_file, deck, deck_file, target_energy, BIN_PATH)
 
     
     print(f"Initial db/de = {db_de_initial:.5f} (b_plus = {b_plus:.5f}, E_plus = {E_plus:.5f}, b_minus = {b_minus:.5f}, E_minus = {E_minus:.5f})")
@@ -87,15 +80,14 @@ def run_bscat_scan(control_file, b_0, input_db, input_de, Emin, Emax, Estart, ta
     bscat = b_0
     b_prev = b_minus
     E_prev = E_minus
-    num_samples_up = int((Emax - Estart) / input_de)
+    num_samples_up = int((E_max - E_start) / input_de)
     db_de = db_de_initial
 
     print("Starting forward walk")
 
-
     for step in range(num_samples_up):
         b_next = bscat + (db_de * input_de)
-        E_next, var_next, path_next, wse_next = opt_E(b_next, forward_ctrl_file, target_energy, "scratch")
+        E_next, var_next, path_next, wse_next = opt_E(b_next, ss, forward_ctrl_file, target_energy, "scratch", BIN_PATH)
         if E_next is None:
             break
         db_de = update_db_de(b_next, b_prev, E_next, E_prev)
@@ -103,7 +95,7 @@ def run_bscat_scan(control_file, b_0, input_db, input_de, Emin, Emax, Estart, ta
             db_de *= -1
         log_step_info(step, "Forward", b_next, db_de, E_next)
 
-        if E_next < Emin or E_next > Emax or abs(db_de) > slope_max:
+        if E_next < E_min or E_next > E_max or abs(db_de) > slope_max:
             break
 
         results.append({
@@ -122,14 +114,14 @@ def run_bscat_scan(control_file, b_0, input_db, input_de, Emin, Emax, Estart, ta
     bscat = b_0
     b_prev = b_plus
     E_prev = E_plus
-    num_samples_down = int((Estart - Emin) / input_de)
+    num_samples_down = int((E_start - E_min) / input_de)
     db_de = db_de_initial
 
     print("Starting backward walk")
 
     for step in range(num_samples_down):
         b_next = bscat - (db_de * input_de)
-        E_next, var_next, path_next, wse_next = opt_E(b_next, backward_ctrl_file, target_energy, "scratch")
+        E_next, var_next, path_next, wse_next = opt_E(b_next, ss, backward_ctrl_file, target_energy, "scratch", BIN_PATH)
         if E_next is None:
             break
         db_de = update_db_de(b_next, b_prev, E_next, E_prev)
@@ -137,7 +129,7 @@ def run_bscat_scan(control_file, b_0, input_db, input_de, Emin, Emax, Estart, ta
             db_de *= -1
         log_step_info(step, "Backward", b_next, db_de, E_next)
 
-        if E_next < Emin or E_next > Emax or abs(db_de) > slope_max:
+        if E_next < E_min or E_next > E_max or abs(db_de) > slope_max:
             break
 
         results.append({
@@ -152,20 +144,30 @@ def run_bscat_scan(control_file, b_0, input_db, input_de, Emin, Emax, Estart, ta
         b_prev = b_next
         E_prev = E_next
 
-    # --- Save and Plot ---
+    # --- Save Results ---
     results_sorted = sorted(results, key=lambda r: r["E_rel"])
     with open("optimized_decks.json", "w") as f:
         json.dump(results_sorted, f, indent=2)
 
-    plt.figure(figsize=(10, 6))
-    plt.plot([r['E_rel'] for r in results_sorted], [r['bscat'] for r in results_sorted], marker='o')
-    plt.xlabel("E_rel (MeV)")
-    plt.ylabel("bscat")
-    plt.title("bscat vs E_rel (Forward + Backward Walks)")
-    plt.grid(True)
-    plt.show()
+    # --- Plotting Results ---
+    # plt.figure(figsize=(10, 6))
+    # plt.plot([r['E_rel'] for r in results_sorted], [r['bscat'] for r in results_sorted], marker='o')
+    # plt.xlabel("E_rel (MeV)")
+    # plt.ylabel("bscat")
+    # plt.title("bscat vs E_rel (Forward + Backward Walks)")
+    # plt.grid(True)
+    # plt.show()
+
+    # --- Clean up working dir ---
+    clean_up_files(forward_ctrl_file, backward_ctrl_file)
 
     return results_sorted
 
-control_file = "he4n_copy.ctrl"
+def clean_up_files(forward_ctrl_file, backward_ctrl_file):
+    
+    os.remove(forward_ctrl_file)
+    os.remove(backward_ctrl_file)
+    shutil.rmtree(Path("scratch"), ignore_errors=True)
+
+# control_file = "he4n_copy.ctrl"
 # run_bscat_scan(control_file, 0.0768, 0.01536, 0.25, 0.5, 5.5, 3, -23.8819, 2)
