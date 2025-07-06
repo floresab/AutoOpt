3b9780d34bb05e3390c92cc7acc241876f51303d
Lydia Mazeeva <l.mazeeva@wustl.edu>
2025-07-06 14:52:44 -0700

    fix: use initial derivative only for initial bscat; same ctrl file for forward/backward walk

--- bscat_optimizer.py
+++ bscat_optimizer.py
@@ -14,35 +14,35 @@ from deck import read_params_and_deck
 from AutoOpt import opt_E
 
 # --- Helper Functions ---
-def estimate_initial_db_de(b_0, ss, input_db, control_file, deck, deck_file, target_energy, BIN_PATH):
+def estimate_initial_db_de(b_0, ss, input_db, control_file, og_deck, og_deck_file, target_energy, cmd, BIN_PATH):
     print("input_db =", input_db)
     b_plus = b_0 + input_db
     b_minus = b_0 - input_db
 
-    control_forward = Control()
-    control_forward.read_control(control_file)
-    control_forward.Name = control_forward.Name + "_forward"
-    control_forward.write_control()
-    forward_file = control_forward.Name + ".ctrl"
+    control = Control()
+    control.read_control(control_file)
 
     print("FORWARD (b_minus)")
-    E_minus, *_ = opt_E(b_minus, ss, forward_file, target_energy, "scratch", BIN_PATH)
-    deck.write_deck(deck_file.strip(".dk"))
+    E_minus, *_ = opt_E(b_minus, ss, control_file, target_energy, "scratch", cmd, BIN_PATH) # <- deck file changes 
 
-    control_backward = Control()
-    control_backward.read_control(control_file)
-    control_backward.Name = control_backward.Name + "_backward"
-    control_backward.write_control()
-    backward_file = control_backward.Name + ".ctrl"
+    # reset control deck 
+    control.parameters['wf'].deck_file = f"'{og_deck_file}'"
+    control.write_control()
+    og_deck.write_deck(og_deck_file.strip(".dk"))
     
     print("BACKWARD (b_plus)")
-    E_plus, *_ = opt_E(b_plus, ss, backward_file, target_energy, "scratch", BIN_PATH)
-    deck.write_deck(deck_file.strip(".dk"))
+    E_plus, *_ = opt_E(b_plus, ss, control_file, target_energy, "scratch", cmd, BIN_PATH)
+
+    # reset control deck 
+    control.parameters['wf'].deck_file = f"'{og_deck_file}'"
+    control.write_control()
+    og_deck.write_deck(og_deck_file.strip(".dk"))
 
     db_de = (b_plus - b_minus) / (E_plus - E_minus)
-    if db_de > 0:                                           # Ensure db_de is negative for the scan
-        db_de *= -1
-    return db_de, b_plus, E_plus, b_minus, E_minus, backward_file, forward_file
+
+    print(f"Initial db/de = {db_de:.5f} (b_plus = {b_plus:.5f}, E_plus = {E_plus:.5f}, b_minus = {b_minus:.5f}, E_minus = {E_minus:.5f})")
+
+    return db_de
 
 def update_db_de(b_new, b_prev, E_new, E_prev):
     return (b_new - b_prev) / (E_new - E_prev)
@@ -51,16 +51,9 @@ def log_step_info(step, direction, bscat, db_de, E_next):
     print(f"[{direction} Step {step}] bscat = {bscat:.5f}, db/de = {db_de:.5f}, E_rel = {E_next:.5f}")
 
 
-def run_bscat_scan(control_file, b_0, ss, input_db, input_de, E_min, E_max, E_start, target_energy, slope_max, BIN_PATH):
+def run_bscat_scan(control_file, b_0, ss, input_db, input_de, E_min, E_max, E_start, target_energy, slope_max, cmd, BIN_PATH):
     
-    E_0, var_0, path_0, wse_0 = opt_E(b_0, ss, control_file, target_energy, "scratch", BIN_PATH)
-
-    scattering_control = Control()
-    scattering_control.read_control(control_file)
-
-    param_file = scattering_control.parameters['wf'].param_file.strip("'\"")
-    deck_file = scattering_control.parameters['wf'].deck_file.strip("'\"")
-    deck, deck_name = read_params_and_deck(param_file, deck_file)
+    E_0, var_0, path_0, wse_0 = opt_E(b_0, ss, control_file, target_energy, "scratch", cmd, BIN_PATH)
 
     results = [{
         "bscat": b_0,
@@ -70,16 +63,21 @@ def run_bscat_scan(control_file, b_0, ss, input_db, input_de, E_min, E_max, E_st
         "wse": wse_0
     }]
 
-    db_de_initial, b_plus, E_plus, b_minus, E_minus, backward_ctrl_file, forward_ctrl_file = estimate_initial_db_de(
-        b_0, ss, input_db, control_file, deck, deck_file, target_energy, BIN_PATH)
+    # Create copy of the control file for backward walk
 
-    
-    print(f"Initial db/de = {db_de_initial:.5f} (b_plus = {b_plus:.5f}, E_plus = {E_plus:.5f}, b_minus = {b_minus:.5f}, E_minus = {E_minus:.5f})")
+    control = Control()
+    control.read_control(control_file)
+
+    param_file = control.parameters['wf'].param_file.strip("'\"")
+    og_deck_file = control.parameters['wf'].deck_file.strip("'\"")
+    og_deck, _ = read_params_and_deck(param_file, og_deck_file)
+
+    db_de_initial = estimate_initial_db_de(b_0, ss, input_db, control_file, og_deck, og_deck_file, target_energy, cmd, BIN_PATH)
 
     # --- Forward Walk ---
     bscat = b_0
-    b_prev = b_minus
-    E_prev = E_minus
+    b_prev = b_0
+    E_prev = E_0
     num_samples_up = int((E_max - E_start) / input_de)
     db_de = db_de_initial
 
@@ -87,12 +85,11 @@ def run_bscat_scan(control_file, b_0, ss, input_db, input_de, E_min, E_max, E_st
 
     for step in range(num_samples_up):
         b_next = bscat + (db_de * input_de)
-        E_next, var_next, path_next, wse_next = opt_E(b_next, ss, forward_ctrl_file, target_energy, "scratch", BIN_PATH)
+        E_next, var_next, path_next, wse_next = opt_E(b_next, ss, control_file, target_energy, "scratch", cmd, BIN_PATH)
+
         if E_next is None:
             break
         db_de = update_db_de(b_next, b_prev, E_next, E_prev)
-        if db_de > 0:                                          # Ensure db_de is negative
-            db_de *= -1
         log_step_info(step, "Forward", b_next, db_de, E_next)
 
         if E_next < E_min or E_next > E_max or abs(db_de) > slope_max:
@@ -111,9 +108,15 @@ def run_bscat_scan(control_file, b_0, ss, input_db, input_de, E_min, E_max, E_st
         E_prev = E_next
 
     # --- Backward Walk ---
+
+    backward_control = copy.deepcopy(control)
+    backward_control.parameters['wf'].deck_file = f"'{og_deck_file}'"
+    og_deck.write_deck(og_deck_file.strip(".dk")) # Reset deck 
+    backward_control.write_control() # overwrite control file
+    
     bscat = b_0
-    b_prev = b_plus
-    E_prev = E_plus
+    b_prev = b_0
+    E_prev = E_0
     num_samples_down = int((E_start - E_min) / input_de)
     db_de = db_de_initial
 
@@ -121,12 +124,11 @@ def run_bscat_scan(control_file, b_0, ss, input_db, input_de, E_min, E_max, E_st
 
     for step in range(num_samples_down):
         b_next = bscat - (db_de * input_de)
-        E_next, var_next, path_next, wse_next = opt_E(b_next, ss, backward_ctrl_file, target_energy, "scratch", BIN_PATH)
+        E_next, var_next, path_next, wse_next = opt_E(b_next, ss, control_file, target_energy, "scratch", cmd, BIN_PATH)
         if E_next is None:
             break
         db_de = update_db_de(b_next, b_prev, E_next, E_prev)
-        if db_de > 0:                                         # Ensure db_de is negative
-            db_de *= -1
+
         log_step_info(step, "Backward", b_next, db_de, E_next)
 
         if E_next < E_min or E_next > E_max or abs(db_de) > slope_max:
@@ -159,15 +161,12 @@ def run_bscat_scan(control_file, b_0, ss, input_db, input_de, E_min, E_max, E_st
     # plt.show()
 
     # --- Clean up working dir ---
-    clean_up_files(forward_ctrl_file, backward_ctrl_file)
+    clean_up_dir()
 
     return results_sorted
 
-def clean_up_files(forward_ctrl_file, backward_ctrl_file):
-    
-    os.remove(forward_ctrl_file)
-    os.remove(backward_ctrl_file)
+def clean_up_dir():
     shutil.rmtree(Path("scratch"), ignore_errors=True)
 
-# control_file = "he4n_copy.ctrl"
-# run_bscat_scan(control_file, 0.0768, 0.01536, 0.25, 0.5, 5.5, 3, -23.8819, 2)
+# control_file = "/Users/lydiamazeeva/QMC/nQMCC/nQMCC/external/nQMCC_Scripts/he4n_copy.ctrl"
+# run_bscat_scan(control_file, 0.0768, '1S[0]', 0.01536, 0.25, 0.5, 5.5, 3, -23.8819, 2, '/Users/lydiamazeeva/QMC/nQMCC/nQMCC/build/bin')
